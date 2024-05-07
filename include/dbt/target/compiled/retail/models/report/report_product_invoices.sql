71af47b0e8906255f7f26508a7d94d99f353a5af
JohnR46 <machine.The@hotmail.com>
2024-05-07 18:12:55 +0700

    add project

---
+++ include/dbt/target/compiled/retail/models/report/report_product_invoices.sql
@@ -0,0 +1,11 @@
+-- report_product_invoices.sql
+SELECT
+  p.product_id,
+  p.stock_code,
+  p.description,
+  SUM(fi.quantity) AS total_quantity_sold
+FROM `southern-sol-421719`.`retail`.`fct_invoices` fi
+JOIN `southern-sol-421719`.`retail`.`dim_product` p ON fi.product_id = p.product_id
+GROUP BY p.product_id, p.stock_code, p.description
+ORDER BY total_quantity_sold DESC
+LIMIT 10
